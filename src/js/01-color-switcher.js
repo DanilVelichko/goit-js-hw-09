e258b4614cd6f575d203c9b1d65881cf7f07e892
DanilVelichko <velichko.danil@gmail.com>
2022-12-14 21:01:31 +0100

    01 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const giveMeBody = document.querySelector('body');
+let timer = null;
+
+startBtn.addEventListener("click", () => {
+    timer = setInterval(() => {
+        giveMeBody.style.backgroundColor = getRandomHexColor();
+    startBtn.setAttribute('disabled', true);    
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timer);
+    startBtn.removeAttribute('disabled');    
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
